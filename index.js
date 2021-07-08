f6b10a0e3b93206894373ca7a544baea1e8318dd
Josh Gerontis <gerontisj@yahoo.com>
2021-07-08 11:58:53 -0600

    working backend

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// server stuff
+/* ENTRY POINT */
+// This file is in charge of starting the application
+
+const server = require("./server");
+
+// define a port
+const port = process.argv[2] || process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Code School 2021 Doge Viewer App`);
+  console.log(`Server Running on port :${port}`);
+});
+
+// more comments
