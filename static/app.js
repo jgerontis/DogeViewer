5e0df9653e049702a1d6bbbd95c62bf73275281d
Raider144 <lawrence.isaac970@gmail.com>
2021-07-08 11:58:46 -0600

    fetch currentPrice

--- static/app.js
+++ static/app.js
@@ -1,15 +1,24 @@
 // vue goes here
-
 var app = new Vue({
     el: "app",
     data: {
         
+        currentPrice: 0,
+
+        server_url = "http://localhost:8080",
     },
-    method: {
+    methods: {
 
     },
     computed: {
-
+        getCurrentPrice:function(){
+            fetch(this.server_url+"/doge").then(function(response){
+                response.json().then(function(data){
+                    app.currentPrice = data.market_data.current_price.usd
+                    console.log(app.currentPrice)
+                })
+            });
+        }
     }
 
 
