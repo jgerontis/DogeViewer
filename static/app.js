d49fab7b733991cf2e73e1a32b922c17990d3fa8
Josh Gerontis <gerontisj@yahoo.com>
2021-07-08 12:16:58 -0600

    working take 3

--- static/app.js
+++ static/app.js
@@ -1,30 +1,23 @@
 // vue goes here
 var app = new Vue({
-    el: "app",
-    vuetify: new Vuetify(),
-    data: {
-        
-        currentPrice: 0,
-
-        server_url = "http://localhost:8080",
+  el: "app",
+  vuetify: new Vuetify(),
+  data: {
+    currentPrice: 0,
+    server_url: "http://localhost:8080",
+  },
+  created: function () {
+    this.getCurrentPrice();
+  },
+  methods: {},
+  computed: {
+    getCurrentPrice: function () {
+      fetch(this.server_url + "/doge").then(function (response) {
+        response.json().then(function (data) {
+          app.currentPrice = data.market_data.current_price.usd;
+          console.log(app.currentPrice);
+        });
+      });
     },
-    created: function () {
-        this.getCurrentPrice();
-    },
-    methods: {
-
-    },
-    computed: {
-        getCurrentPrice:function(){
-            fetch(this.server_url+"/doge").then(function(response){
-                response.json().then(function(data){
-                    app.currentPrice = data.market_data.current_price.usd
-                    console.log(app.currentPrice)
-                })
-            });
-        }
-    }
-
-
-
-})
+  },
+});
